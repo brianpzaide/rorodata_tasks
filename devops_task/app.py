1d27bf152d7e370f04d24091bf270f76b2e5d3e3
brianpzaide <brianpzaide@gmail.com>
2025-08-09 02:10:08 +0000

    devops_task initial commit

---
+++ devops_task/app.py
@@ -0,0 +1,20 @@
+import random
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/more-items")
+def more_items():
+    items = []
+    for _ in range(0, random.randint(1, 11)):
+        items.append("".join(random.choices("abcdefghijklmnopqrstuvwxyz", k=random.randint(4, 11))))
+    return render_template("partials/more_items.html", items=items)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
